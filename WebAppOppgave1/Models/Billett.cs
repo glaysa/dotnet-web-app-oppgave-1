8a61811771b128dbf0754ec3db3cc2eb68499c43
NimaAB <nimabewrani98@gmail.com>
2021-09-13 12:14:58 +0200

    endret Bestilling-model til Billett

---
+++ WebAppOppgave1/Models/Billett.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppOppgave1.Models
+{
+    public class Billett
+    {
+        public int BillettID { set; get; }
+        public int KundeID { get; }
+        //usikker om vi lagrer idene eller hele objektet.
+        public int [] Lugarer { set; get; }
+        public int [] Meals { set; get; }
+        public int [] SPA { set; get; }
+    }
+}
