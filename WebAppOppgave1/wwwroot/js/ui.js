1ac79d1b42469429ffb18203d3e8dce625ee67b0
Glaysa <glaysa.df@gmail.com>
2021-09-13 15:29:50 +0200

    created front page

---
+++ WebAppOppgave1/wwwroot/js/ui.js
@@ -0,0 +1,39 @@
+﻿
+////////////////////////////////////////////////////////////////////
+/////                                                          /////
+/////        Denne filen gjør nettsiden dynamisk.              /////
+/////    Ingen server relatert logikk er implementert her.     /////
+/////                                                          /////
+////////////////////////////////////////////////////////////////////
+
+// Ready funksjon
+$(document).ready(function () {
+    visKalender();
+});
+
+// Endrer meny ikonen på små enheter fra burger til X og omvendt.
+$("#btn-toggle").click(function () {
+    $("#btn-toggle").toggleClass("on");
+});
+
+// Viser kalender når dato input er klikket
+function visKalender(){
+    $('#fra-dato, #til-dato').datepicker({
+        beforeShow: customRange,
+        dateFormat: "dd. M yy",
+    });
+}
+
+// Fjerner ugyldige datoer fra dato inputs
+function customRange(input) {
+    if (input.id === 'fra-dato') {
+        let minDate = new Date();
+        minDate.setDate(minDate.getDate())
+        return { minDate: minDate };
+    }
+    if (input.id === 'til-dato') {
+        let minDate = new Date($('#fra-dato').val());
+        minDate.setDate(minDate.getDate() + 1)
+        return { minDate: minDate };
+    }
+}
