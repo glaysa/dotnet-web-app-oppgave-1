c306c1b2ee2e6fc68fcf880ffc6bfd5939566c77
NimaAB <nimabewrani98@gmail.com>
2021-09-13 11:35:03 +0200

    litt jobb med modelene

---
+++ WebAppOppgave1/Models/Kunde.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppOppgave1.Models
+{
+    public class Kunde
+    {
+        public int kundeID { set; get; }
+        public string fornavn { set; get; }
+        public string etternavn { set; get; }
+        public string tlfnummer { set; get; }
+        public string epost { set; get; }
+        public string adresse { set; get; }
+        public string postnummer { set; get; }
+        public string poststed { set; get; }
+    }
+}
