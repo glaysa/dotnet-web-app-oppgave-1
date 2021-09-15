473d6f425323023e9dea18a50c53d4148510d0bd
NimaAB <nimabewrani98@gmail.com>
2021-09-15 20:14:07 +0200

    lagrer billett

--- WebAppOppgave1/Models/Billett.cs
+++ WebAppOppgave1/Models/Billett.cs
@@ -9,15 +9,14 @@ namespace WebAppOppgave1.Models
     {
         public int BillettID { set; get; }
         public int KundeID { get; }
-        //usikker om vi lagrer idene eller hele objektet.
-        public Lugar [] Lugarer { set; get; }
-        public int [] Meals { set; get; }
-        public int [] SPA { set; get; }
         public string Rute { set; get; }
         public DateTime UtreiseDato { set; get; }
         public DateTime UtreiseTid { set; get; }
         public DateTime AnkomstDato { set; get; }
         public DateTime AnkomstTid { set; get; }
         public double Pris { set; get; }
+        public Lugar[] Lugarer { set; get; }
+        public int[] Meals { set; get; }
+        public int[] SPA { set; get; }
     }
 }
