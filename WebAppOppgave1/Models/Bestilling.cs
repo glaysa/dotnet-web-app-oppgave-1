6c2aa7653466305cbbe2cddd23e04236b135e03a
NimaAB <nimabewrani98@gmail.com>
2021-09-16 15:40:56 +0200

    Et alternativ for model og database arkitekturen

---
+++ WebAppOppgave1/Models/Bestilling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppOppgave1.Models
+{
+    public class Bestilling
+    {
+        public int BestillingNummer { set; get; }
+        public int KundeID { set; get; }
+        public int [] BillettID { set; get; }
+        public int [] LugarNummer { set; get; }
+        public int [] MealsNummer { set; get; }
+        public double TotalPris { set; get; }
+    }
+}
---
+++ WebAppOppgave1/Models/Meal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppOppgave1.Models
+{
+    public class Meal
+    {
+    }
+}
